ee129885eb72c0a6e96e420973d8b82576e22a52
aurogarcia <gabriellsamy08@gmail.com>
2025-06-12 20:29:04 -0300

    questao2

--- projeto/todos/views.py
+++ projeto/todos/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import ListView, CreateView, UpdateView
+from .models import Todo
 
 # Create your views here.
+class TodoListView(ListView):
+    model = Todo
+    
